10146854ac8b34923e8b1b964e586193629a9ea0
Husky-Yellow <473795376@qq.com>
2021-08-03 23:23:59 +0800

    feat:creat project by张照坤 新建项目

---
+++ config/config.default.js
@@ -0,0 +1,41 @@
+/* eslint valid-jsdoc: "off" */
+
+'use strict';
+
+/**
+ * @param {Egg.EggAppInfo} appInfo app info
+ */
+module.exports = appInfo => {
+  /**
+   * built-in config
+   * @type {Egg.EggAppConfig}
+   **/
+  const config = exports = {};
+
+  // use for cookie sign key, should change to your own and keep security
+  config.keys = appInfo.name + '_1627999715428_239';
+
+  // add your middleware config here
+  config.middleware = [];
+
+  // add your user config here
+  const userConfig = {
+    // myAppName: 'egg',
+  };
+
+  config.security = {
+    csrf: {
+      enable: false,
+      ignoreJSON: true
+    },
+    domainWhiteList: [ '*' ], // 配置白名单
+  };
+  config.view = {
+    mapping: {'.html': 'ejs'}  //左边写成.html后缀，会自动渲染.html文件
+  };
+
+  return {
+    ...config,
+    ...userConfig,
+  };
+};
